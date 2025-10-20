b1e63bf59397286d71a6e86a691a19139d26f75b
Jose <>
2025-10-20 17:53:17 +0200

    Añadido movimiento

--- Practica1/Assets/Scripts/Player.cs
+++ Practica1/Assets/Scripts/Player.cs
@@ -4,9 +4,9 @@ public class Player : MonoBehaviour
 {
 
     public int velocidad;
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
 
-
+    //[RequireComponent(typeof(Rigidbody2D))]
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,8 +20,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*if (Input.GetKey(KeyCode.A));
+        {
+
+            transform.Translate(-velocidad * Time.deltaTime,0,0);
+
+        }
+
+        */
 
         rb.linearVelocity = new Vector2(Input.GetAxisRaw("Horizontal") * velocidad, rb.linearVelocity.y);
 
+
+
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Damage"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
 }
